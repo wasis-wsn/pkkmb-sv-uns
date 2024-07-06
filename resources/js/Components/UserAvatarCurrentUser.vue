97613fd52e4a0fc1b60e1834eb01bef6d0f1d5a0
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-06 15:04:30 +0700

    dashboard template

---
+++ resources/js/Components/UserAvatarCurrentUser.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/vue3'
+import { useMainStore } from '@/Stores/main'
+import UserAvatar from '@/Components/UserAvatar.vue'
+
+const pageProps = usePage().props
+const userName = computed(() => pageProps.auth.user ? pageProps.auth.user.name : '')
+
+const mainStore = useMainStore()
+</script>
+
+<template>
+  <UserAvatar :username="userName" api="initials">
+    <slot />
+  </UserAvatar>
+</template>
