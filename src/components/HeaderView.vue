0da73f83491b994410bf7abdcc3d00dce0595e51
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-08 11:57:55 +0700

    update

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+      <nav>
+        <!-- Isi navigasi header di sini -->
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderView'
+    // Komponen header bisa memiliki properti, metode, atau logika lainnya sesuai kebutuhan
+  };
+  </script>
+  
+  <style scoped>
+  /* Styling untuk header */
+  header {
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+  }
+  nav {
+    display: flex;
+    justify-content: space-between;
+  }
+  nav a {
+    color: white;
+    text-decoration: none;
+    margin-right: 1rem;
+  }
+  </style>
+  
---
+++ src/components/LayoutWithHeaderFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <HeaderView />
+      
+      <!-- Konten dari halaman yang menggunakan layout ini -->
+      <slot></slot>
+      
+      <FooterView />
+    </div>
+  </template>
+  
+  <script>
+import HeaderView from './HeaderView.vue';
+import FooterView from './FooterView.vue';
+  
+  export default {
+    components: {
+      HeaderView,
+      FooterView
+    }
+  };
+  </script>
+  
