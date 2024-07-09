5a537cb68f6e34e9a35c619fc14b76a59a5e3892
WageAriel <132081758+WageAriel@users.noreply.github.com>
2024-07-09 13:00:17 +0700

    Edit Materi

--- src/pages/MateriView copy.vue
+++ src/pages/MateriView copy.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="flex flex-col bg-white">
-    <section class="flex gap-5 w-full max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
+   <div class="container mx-auto">
+    <main>
+      <section class="flex gap-5 w-full max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
         <div
           class="flex flex-col flex-1 py-20 rounded-full border border-solid border-violet-300 border-opacity-50 max-md:max-w-full">
           <div class="flex flex-col mt-20 max-w-full w-[200px] max-md:mt-10">
@@ -18,13 +19,11 @@
         <img loading="lazy" src="../assets/Materi/LOGO-COLOR 2.png" alt="PKKMB SV UNS 2024 illustration"
           class="flex-1 my-auto w-full aspect-[0.96] max-md:max-w-full" />
       </section>
-    <main>
-      <section class="flex gap-5 self-center ml-16 mt-[200px] max-md:flex-wrap max-md:mt-10">
+
+      <section class="flex gap-5 self-center ml-20 mt-[300px] max-md:flex-wrap max-md:mt-10">
         <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
           <h2 class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[630px]">
-            <span class="max-md:max-w-full">
-              BUKU IMPLEMENTASI NILAI ASKARA MUDA<br />PKKMB SV UNS 2024
-            </span>
+            BUKU IMPLEMENTASI NILAI ASKARA MUDA<br />PKKMB SV UNS 2024
             <div class="shrink-0 mt-2.5 h-1 bg-red-400 max-md:max-w-full"></div>
           </h2>
           <p class="mt-8 text-2xl text-stone-900 max-md:max-w-full">
@@ -39,16 +38,17 @@
           <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"></div>
           <div class="shrink-0 self-end mt-12 bg-red-300 bg-opacity-20 h-[150px] rounded-[100px] w-[5px] max-md:mt-10">
           </div>
-          <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"></div>
+          <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] 50w-[5px]"></div>
         </div>
       </section>
+
       <section
-        class="relative flex justify-center items-center pt-16 pb-32 w-full text-2xl font-bold text-black bg-white">
+        class="relative flex justify-center items-center pt-16 pb-32 w-full text-2xl font-bold text-black bg-white max-md:px-5 max-md:mt-10 max-md:max-w-full">
         <!-- Background Image -->
         <img loading="lazy" src="../assets/Materi/Materi.png" alt="Decorative gradient"
-          class="absolute top-0 left-0 z-0 w-full h-auto max-w-full max-h-full " />
+          class="absolute ml-10 top-0 left-0 z-0 w-full h-auto max-w-full max-h-full scale-110" />
         <img loading="lazy" src="../assets/Materi/materi2.png" alt="Decorative gradient"
-          class="absolute mt-96 -top-10 left-0 z-0 w-full h-auto max-w-full max-h-full " />
+          class="absolute mt-96 ml-10 top-0 left-0 z-0 w-full h-auto max-w-full max-h-full scale-110" />
 
         <!-- Content -->
         <div
@@ -86,30 +86,24 @@
         </div>
 
       </section>
-      <section class="flex gap-5 self-center mt-[200px] max-md:flex-wrap max-md:mt-10">
-        <div class="flex z-20 flex-col w-[33%] max-md:ml-0 max-md:w-full -mt-[600px]">
-          <img loading="lazy" src="../assets/Materi/Materi3.png" alt="Decorative image"
-             />
-        </div>
+      <!-- TWIBBON -->
+      <section class="relative z-10 flex gap-5 self-center ml- mt-[200px] max-md:flex-wrap max-md:mt-10">
         <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
-          <h2 class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[630px]">
-            <span class="max-md:max-w-full">
-              BUKU IMPLEMENTASI NILAI ASKARA MUDA<br />PKKMB SV UNS 2024
-            </span>
+          <h2 class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[180px]">
+            TWIBBON
             <div class="shrink-0 mt-2.5 h-1 bg-red-400 max-md:max-w-full"></div>
           </h2>
         </div>
-        <div class="flex z-20 flex-col w-[33%] max-md:ml-0 max-md:w-full -mt-[588px]">
-          <img loading="lazy" src="../assets/Materi/Materi4.png" alt="Decorative image"
-           />
-        </div>
       </section>
       <section
-        class="relative flex justify-center items-center pt-16 pb-32 w-full text-2xl font-bold text-black bg-white">
+        class="relative flex justify-center items-center px-16 pt-16 pb-32 w-full text-2xl font-bold text-black bg-white max-md:px-5 max-md:mt-10 max-md:max-w-full">
+        <!-- Background Image -->
+        <img loading="lazy" src="../assets/Materi/WAVETWIBBON.png" alt="Decorative gradient"
+          class="absolute -mt-96 -translate-y-64 ml-10 top-0 left-0 z-0 w-full h-auto max-w-full max-h-full scale-110" />
 
         <!-- Content -->
         <div
-          class="relative z-10 justify-center -mt-[250px] px-20 pt-20 mb-0 w-full bg-slate-100 max-w-[1160px] pb-[100px] rounded-[60px] max-md:pr-8 max-md:pb-10 max-md:pl-5 max-md:mb-2.5 max-md:max-w-full">
+          class="relative z-10 justify-center px-20 pt-20 mb-0 w-full bg-slate-100 max-w-[1160px] pb-[100px] rounded-[60px] max-md:pr-8 max-md:pb-10 max-md:pl-5 max-md:mb-2.5 max-md:max-w-full">
           <h3>Petunjuk Umum Implementasi Nilai :</h3>
           <ul>
             <li>Askara Muda adalah sebutan untuk Mahasiswa Baru yang mengikuti PKKMB SV UNS Askara 2023.</li>
@@ -143,15 +137,30 @@
         </div>
 
       </section>
-      <!-- <section class=" mt-16 w-full max-md:mt-10 max-md:max-w-full">
-        <div class="flex gap-5 -mt-96 max-md:flex-col max-md:gap-0">
+
+
+      <section class="px-5 mt-16 w-full max-md:mt-10 max-md:max-w-full">
+        <div class="flex gap-5 max-md:flex-col max-md:gap-0">
+          <div class="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+            <img loading="lazy" src="../assets/Materi/Materi3.png" alt="Decorative image"
+              class="grow w-full aspect-[0.91] fill-[linear-gradient(127deg,#E39043_16.19%,#4A3A99_67.07%)] max-md:mt-10" />
+          </div>
           <div class="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <div class="flex flex-col self-stretch my-auto text-4xl text-center capitalize max-md:mt-10">
+              <div class="font-bold text-black border border-black border-solid">Download materi</div>
+              <a href="#"
+                class="justify-center items-center px-10 py-3.5 mt-10 font-extrabold text-white whitespace-nowrap bg-slate-700 rounded-[30px] max-md:px-5">
+                DISINI!
+              </a>
+            </div>
+          </div>
+          <div class="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+            <img loading="lazy" src="../assets/Materi/Materi4.png" alt="Decorative image"
+              class="grow w-full aspect-[0.93] fill-[linear-gradient(127deg,#E39043_16.19%,#4A3A99_67.07%)] max-md:mt-10" />
           </div>
         </div>
-      </section> -->
-      
+      </section>
     </main>
-
   </div>
 </template>
 
