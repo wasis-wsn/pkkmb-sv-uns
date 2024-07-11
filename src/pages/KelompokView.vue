52aabc40a86b4929692070c5a2e8f9bb736b8e99
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-11 14:37:12 +0700

    update

--- src/pages/KelompokView.vue
+++ src/pages/KelompokView.vue
@@ -1,173 +1,259 @@
 <template>
   <div class="flex flex-col bg-white">
-      <header class="flex gap-5 max-md:flex-col max-md:gap-0">
-        <section class="flex flex-col flex-1 items-start pb-32 mt-16 ml-2 rounded-full border border-solid border-violet-300 border-opacity-50 max-md:max-w-full">
-            <div class="shrink-0 mt-20 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[180px] max-md:mt-10"></div>
-            <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[120px] max-md:mt-10"></div>
-            <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[190px]"></div>
-            <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[150px]"></div>
-            <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[200px]"></div>
-            <div class="self-end mt-1 mb-0 max-md:max-w-full">
-                <h1 class="self-stretch pl-8 mt-5 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-10 max-md:max-w-full">
-                    <span class="font-semibold">Selamat Datang di </span></h1>
-                <h1 class="self-stretch pl-8 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-0 max-md:max-w-full">
-                    <span class="text-5xl text-red-400">PKKMB SV UNS 2024</span>.
-                </h1>
-            </div>
-        </section>
-        <section class="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-            <img loading="lazy" src="@/assets/logo_besar.png" alt="" class="box-border object-cover overflow-hidden shrink-0 mt-0 w-full aspect-[0.99] min-h-[20px] min-w-[20px]" />
-        </section>
-      </header>
-      <div class="flex gap-5 self-end px-5 mt-0 max-md:mt-10">
+    <header class="flex gap-5 max-md:flex-col max-md:gap-0">
+      <section class="flex flex-col flex-1 items-start pb-32 mt-16 ml-2 rounded-full border border-solid border-violet-300 border-opacity-50 max-md:max-w-full">
+        <div class="shrink-0 mt-20 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[180px] max-md:mt-10"></div>
+        <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[120px] max-md:mt-10"></div>
+        <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[190px]"></div>
+        <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[150px]"></div>
+        <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[200px]"></div>
+        <div class="self-end mt-1 mb-0 max-md:max-w-full">
+          <h1 class="self-stretch pl-8 mt-5 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-10 max-md:max-w-full">
+            <span class="font-semibold">Kelompok di </span></h1>
+          <h1 class="self-stretch pl-8 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-0 max-md:max-w-full">
+            <span class="text-5xl text-red-400">PKKMB SV UNS 2024</span>.
+          </h1>
+        </div>
+      </section>
+      <section class="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+        <img loading="lazy" src="@/assets/logo_besar.png" alt="" class="box-border object-cover overflow-hidden shrink-0 mt-0 w-full aspect-[0.99] min-h-[20px] min-w-[20px]" />
+      </section>
+    </header>
+    <div class="flex gap-5 self-end px-5 mt-0 max-md:mt-10">
         <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"></div>
         <div class="shrink-0 self-end mt-16 bg-red-300 bg-opacity-20 h-[130px] rounded-[100px] w-[5px] max-md:mt-10"></div>
         <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"></div>
         <div class="shrink-0 self-end mt-12 bg-red-300 bg-opacity-20 h-[150px] rounded-[100px] w-[5px] max-md:mt-10"></div>
         <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"></div>
-      </div>
-  </div>
-  <section class="flex justify-center items-center px-16 py-20 -mt-1 w-full bg-slate-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
+    </div>
+
+    <section class="flex justify-center items-center px-16 py-20 -mt-1 w-full bg-slate-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
       <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
-        <h2
-          class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[650px] max-md:w-full">
-          <span class="max-md:max-w-full">
-            BUKU IMPLEMENTASI NILAI ASKARA MUDA<br />PKKMB SV UNS 2024
-          </span>
+        <h2 class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[650px] max-md:w-full">
+          <span class="max-md:max-w-full">KELOMPOK<br />PKKMB SV UNS 2024</span>
           <div class="shrink-0 mt-2.5 h-1 bg-red-400 max-md:max-w-full"></div>
         </h2>
         <p class="mt-8 z-20 text-2xl text-stone-900 max-md:max-w-full max-md:text-xl text-[18px] md:text-[24px]">
-          Hallo Vocazens! Materi ini berisi Buku Implementasi Nilai<br />Askara Muda yang digunakan sebagai materi dalam
-          kegiatan
-          <br />PKKMB SV UNS 2024.
+          Hallo Vocazens! Di bawah ini adalah informasi tentang kelompok dan<br> nama-nama mahasiswa di PKKMB SV UNS 2024.
         </p>
-        <div class="bg-white p-4 rounded-lg shadow-md overflow-x-auto">
-  <form class="search-form flex items-center gap-2 mb-4" @submit.prevent>
-    <div class="relative w-60">
-      <div class="relative grid grid-cols-[auto_1fr] items-center border border-gray-300 rounded-lg p-1 focus-within:ring-2 focus-within:ring-blue-500">
-        <input type="text" id="groupSearch" class="search-input w-full pl-2 py-2 focus:outline-none" placeholder="Masukkan nama anda" v-model="searchQuery" @input="handleSearch" />
-        <Search v-if="!isSearching" />
-      </div>
-    </div>
-    <div class="relative">
-      <Filter @click="toggleDropdown" />
-      <div v-show="showDropdown" class="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-md">
-        <div class="py-2">
-          <div class="block px-4 py-2 text-gray-800 hover:bg-gray-100 w-full text-left cursor-pointer" v-for="kel in kelompok" :key="kel.id" @click="filterByKelompok(kel.kelompok)">{{ kel.kelompok }}</div>
+
+        <!-- Search form -->
+        <div class="bg-white p-4 mt-8 rounded-lg shadow-md overflow-x-auto">
+          <form class="search-form flex items-center gap-2 mb-4" @submit.prevent>
+            <div class="relative flex items-center border border-gray-300 rounded-lg p-1 focus-within:ring-2 focus-within:ring-blue-500">
+              <input type="text" id="groupSearch" class="search-input w-full pl-3 py-2 focus:outline-none" placeholder="Cari dirimu" v-model="searchQuery" @input="handleSearch" />
+            </div>
+            <div class="relative">
+              <RiEqualizerLine class="h-5 w-5 text-gray-400 absolute right-3 top-1/2 transform -translate-y-1/2" />              
+              <select v-model="selectedKelompok" @change="filterByKelompok" class="block appearance-none w-full bg-white border border-gray-300 rounded-lg shadow-md py-2 pl-3 pr-10 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                <option value="">Select All</option>
+                <option v-for="kel in kelompokList" :key="kel.id" :value="kel.kelompok">{{ kel.kelompok }}</option>
+              </select>
+            </div>
+          </form>
+
+          <!-- Table displaying users -->
+          <table class="group-table w-full border-collapse">
+            <thead>
+              <tr class="bg-blue-100">
+                <th class="py-2 px-4 text-center border">Nama</th>
+                <th class="py-2 px-4 text-center border">Prodi</th>
+                <th class="py-2 px-4 text-center border">Kelompok</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in paginatedUsers" :key="user.id" class="border-b">
+                <td class="py-2 px-4 text-center">{{ user.nama }}</td>
+                <td class="py-2 px-4 text-center">{{ user.prodi }}</td>
+                <td class="py-2 px-4 text-center">{{ user.kelompok }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <!-- Pagination controls -->
+          <div class="flex justify-center mt-4">
+            <button @click="prevPage" :disabled="currentPage === 1" class="mx-1 px-3 py-1 bg-primary text-white rounded disabled:opacity-50">Prev</button>
+            <span class="mx-2">{{ currentPage }} / {{ totalPages }}</span>
+            <button @click="nextPage" :disabled="currentPage === totalPages" class="mx-1 px-3 py-1 bg-primary text-white rounded disabled:opacity-50">Next</button>
+          </div>
         </div>
       </div>
-    </div>
-  </form>
-  <table class="group-table w-full border-collapse">
-    <thead>
-      <tr class="bg-blue-100 ">
-        <th class="py-2 px-4 text-center border">Nama</th>
-        <th class="py-2 px-4 text-center border">Prodi</th>
-        <th class="py-2 px-4 text-center border">Kelompok</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="user in paginatedUsers" :key="user.id" class="border-b">
-        <td class="py-2 px-4 text-center">{{ user.nama }}</td>
-        <td class="py-2 px-4 text-center">{{ user.prodi }}</td>
-        <td class="py-2 px-4 text-center">{{ user.kelompok }}</td>
-      </tr>
-    </tbody>
-  </table>
-  <div class="flex justify-center mt-4">
-    <button @click="prevPage" :disabled="currentPage === 1" class="mx-1 px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-50">Prev</button>
-    <span class="mx-2">{{ currentPage }} / {{ totalPages }}</span>
-    <button @click="nextPage" :disabled="currentPage === totalPages" class="mx-1 px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-50">Next</button>
+    </section>
   </div>
-</div>
-
-      </div>
-  </section>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue';
-import { Search, Filter } from 'lucide-vue-next';
+import { RiEqualizerLine } from "@remixicon/vue";
 
 // Sample data, replace with actual data
-const users = [{
-  id: 1,
-  nama: 'yoga',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'macan aung'
-},
-{
-  id: 2,
-  nama: 'ardya',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'bangau wayaw'
-},
-{
-  id: 3,
-  nama: 'mamat',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'elang putra'
-},
-{
-  id: 4,
-  nama: 'mamat',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'elang putra'
-},
-{
-  id: 5,
-  nama: 'mamat',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'elang putra'
-},
-{
-  id: 6,
-  nama: 'mamat',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'elang putra'
-},
-{
-  id: 7,
-  nama: 'mamat',
-  prodi: 'd3 teknik informatika',
-  kelompok: 'elang putra'
-},
+const users = [
+  {
+    id: 1,
+    nama: 'yoga',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'macan aung'
+  },
+  {
+    id: 2,
+    nama: 'ardya',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'bangau wayaw'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  {
+    id: 3,
+    nama: 'mamat',
+    prodi: 'd3 teknik informatika',
+    kelompok: 'elang putra'
+  },
+  // More users data here
 ];
 
-const kelompok = [{
-  id: 1,
-  kelompok: 'macan aung'
-},
-{
-  id: 2,
-  kelompok: 'bangau wayaw'
-},
-{
-  id: 3,
-  kelompok: 'elang putra'
-},
+const kelompokList = [
+  {
+    id: 1,
+    kelompok: 'macan aung'
+  },
+  {
+    id: 2,
+    kelompok: 'bangau wayaw'
+  },
+  {
+    id: 3,
+    kelompok: 'elang putra'
+  },
+  // More kelompok data here
 ];
 
 const searchQuery = ref('');
 const selectedKelompok = ref('');
-const showDropdown = ref(false);
-const isSearching = ref(false);
-
 const currentPage = ref(1);
 const itemsPerPage = 20;
 
-const toggleDropdown = () => {
-  showDropdown.value = !showDropdown.value;
-};
-
-const filterByKelompok = (kel) => {
-  selectedKelompok.value = kel;
-  showDropdown.value = false;
-};
-
 const handleSearch = () => {
-  isSearching.value = searchQuery.value.length > 0;
   currentPage.value = 1; // Reset to first page when search query changes
+};
+
+const filterByKelompok = (user) => {
+  if (!selectedKelompok.value) {
+    return true; // Show all users if no kelompok is selected
+  } else {
+    return user.kelompok === selectedKelompok.value;
+  }
 };
 
 const filteredUsers = computed(() => {
@@ -180,10 +266,7 @@ const filteredUsers = computed(() => {
       user.kelompok.toLowerCase().includes(lowercasedQuery)
     );
   }
-  if (selectedKelompok.value) {
-    result = result.filter(user => user.kelompok === selectedKelompok.value);
-  }
-  return result;
+  return result.filter(filterByKelompok);
 });
 
 const paginatedUsers = computed(() => {
@@ -226,5 +309,9 @@ const nextPage = () => {
       max-width: 100px; /* Adjust as needed for smaller screens */
     }
   }
+  .search-input:focus + label {
+    top: 1px; /* Adjust as needed to align perfectly */
+    font-size: 12px; /* Adjust label font size when input is focused */
+    color: #333; /* Adjust label color when input is focused */
+  }
 </style>
-            wqaaezrsdfcvxwatbryhdgvfuygijmhkn bj bmnv
