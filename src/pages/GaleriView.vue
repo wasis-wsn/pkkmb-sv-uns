280667df567d42ca7f4d1e698abe56261357a2b6
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-11 19:00:32 +0700

    update

--- src/pages/GaleriView.vue
+++ src/pages/GaleriView.vue
@@ -361,45 +361,43 @@ register();
 export default {
   name: "GaleriView",
   mounted() {
-  console.log('Initializing Swiper...');
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    direction: 'horizontal',
-    slidesPerView: 1,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+    new Swiper(".swiper", {
+      loop: true,
+      direction: "horizontal",
+      slidesPerView: 1,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 2000, 
+        disableOnInteraction: false,
       },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
       },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 40,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-      1280: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  console.log('Swiper initialized:', swiper);
-}
+    });
+  },
 };
 </script>
 
