5f4455e6c0c9a90d72b5f8a7769caff8024fe741
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-14 13:00:57 +0700

    update

--- src/pages/LandingView.vue
+++ src/pages/LandingView.vue
@@ -1,51 +1,30 @@
 <template>
-<div class="flex flex-col bg-white">
-    <header class="flex gap-5 max-md:flex-col max-md:gap-0">
-      <section
-        class="flex flex-col flex-1 items-start pb-32 mt-16 ml-2 rounded-full border border-solid border-violet-300 border-opacity-50 max-md:max-w-full"
-        data-aos="fade-right"
-      >
+  <header class="flex gap-5 max-md:flex-col max-md:gap-0">
+    <section
+      class="flex flex-col flex-1 items-start pb-32 mt-16 ml-2 rounded-full border border-solid border-violet-300 border-opacity-50 max-md:max-w-full"
+      data-aos="fade-right">
+      <div class="shrink-0 mt-20 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[180px] max-md:mt-10"
+        data-aos="fade-right" data-aos-delay="100"></div>
+      <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[120px] max-md:mt-10"
+        data-aos="fade-right" data-aos-delay="200"></div>
+      <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[190px]" data-aos="fade-right"
+        data-aos-delay="300"></div>
+      <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[150px]" data-aos="fade-right"
+        data-aos-delay="400"></div>
+      <div class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[200px]" data-aos="fade-right"
+        data-aos-delay="500"></div>
+      <div class="self-end mt-1 mb-0 max-md:max-w-full">
+        <h1
+          class="self-stretch pl-8 mt-5 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-10 max-md:max-w-full"
+          data-aos="fade-right" data-aos-delay="600">
+          <span class="font-semibold">Selamat Datang di.</span>
+        </h1>
+        <h1
+          class="self-stretch pl-8 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-0 max-md:max-w-full"
+          data-aos="fade-right" data-aos-delay="700">
+          <span class="text-5xl text-red-400">PKKMB SV UNS 2024</span>.
+        </h1>
         <div
-          class="shrink-0 mt-20 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[180px] max-md:mt-10"
-          data-aos="fade-right"
-          data-aos-delay="100"
-        ></div>
-        <div
-          class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[120px] max-md:mt-10"
-          data-aos="fade-right"
-          data-aos-delay="200"
-        ></div>
-        <div
-          class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[190px]"
-          data-aos="fade-right"
-          data-aos-delay="300"
-        ></div>
-        <div
-          class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[150px]"
-          data-aos="fade-right"
-          data-aos-delay="400"
-        ></div>
-        <div
-          class="shrink-0 mt-5 bg-red-300 bg-opacity-20 h-[5px] rounded-[100px] w-[200px]"
-          data-aos="fade-right"
-          data-aos-delay="500"
-        ></div>
-        <div class="self-end mt-1 mb-0 max-md:max-w-full">
-          <h1
-            class="self-stretch pl-8 mt-5 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-10 max-md:max-w-full"
-            data-aos="fade-right"
-            data-aos-delay="600"
-          >
-            <span class="font-semibold">Selamat Datang di</span>
-          </h1>
-          <h1
-            class="self-stretch pl-8 -mr-1 ml-8 text-4xl font-bold leading-[60px] text-neutral-900 max-md:mt-0 max-md:max-w-full"
-            data-aos="fade-right"
-            data-aos-delay="700"
-          >
-            <span class="text-5xl text-red-400">PKKMB SV UNS 2024</span>.
-          </h1>
-          <div
             class="flex justify-end gap-5 font-extrabold text-center max-md:mt-10 max-md:mr-2.5"
             data-aos="fade-right"
             data-aos-delay="800"
@@ -65,49 +44,28 @@
               <span class="my-auto">Materi</span>
             </router-link>
           </div>
-        </div>
-      </section>
-      <section
-        class="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full"
-        data-aos="fade-up"
-      >
-        <img
-          loading="lazy"
-          src="@/assets/logo_besar.png"
-          alt=""
-          class="box-border object-cover overflow-hidden shrink-0 mt-0 w-full aspect-[0.99] min-h-[20px] min-w-[20px]"
-        />
-      </section>
-    </header>
+      </div>
+    </section>
+    <section class="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full" data-aos="fade-up">
+      <img loading="lazy" src="@/assets/logo_besar.png" alt=""
+        class="box-border object-cover overflow-hidden shrink-0 mt-0 w-full aspect-[0.99] min-h-[20px] min-w-[20px]" />
+    </section>
+  </header>
+  <div class="flex flex-col bg-white">
     <div class="flex gap-5 self-end px-5 mt-0 max-md:mt-10">
-      <div
-        class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"
-        data-aos="fade-up"
-        data-aos-delay="100"
-      ></div>
-      <div
-        class="shrink-0 self-end mt-16 bg-red-300 bg-opacity-20 h-[130px] rounded-[100px] w-[5px] max-md:mt-10"
-        data-aos="fade-up"
-        data-aos-delay="200"
-      ></div>
-      <div
-        class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"
-        data-aos="fade-up"
-        data-aos-delay="300"
-      ></div>
-      <div
-        class="shrink-0 self-end mt-12 bg-red-300 bg-opacity-20 h-[150px] rounded-[100px] w-[5px] max-md:mt-10"
-        data-aos="fade-up"
-        data-aos-delay="400"
-      ></div>
-      <div
-        class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]"
-        data-aos="fade-up"
-        data-aos-delay="500"
-      ></div>
+      <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]" data-aos="fade-up"
+        data-aos-delay="100"></div>
+      <div class="shrink-0 self-end mt-16 bg-red-300 bg-opacity-20 h-[130px] rounded-[100px] w-[5px] max-md:mt-10"
+        data-aos="fade-up" data-aos-delay="200"></div>
+      <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]" data-aos="fade-up"
+        data-aos-delay="300"></div>
+      <div class="shrink-0 self-end mt-12 bg-red-300 bg-opacity-20 h-[150px] rounded-[100px] w-[5px] max-md:mt-10"
+        data-aos="fade-up" data-aos-delay="400"></div>
+      <div class="shrink-0 bg-red-300 bg-opacity-20 h-[200px] rounded-[100px] w-[5px]" data-aos="fade-up"
+        data-aos-delay="500"></div>
     </div>
   </div>
-  <main>
+  <div class="flex flex-col bg-white">
     <div class="flex justify-center items-center">
       <section
         v-for="(movie, index) in comingSoon"
@@ -174,10 +132,17 @@
             <article class="flex flex-col max-w-[192px] mx-auto">
               <div class="relative mt-5" data-aos="fade-left">
                 <!-- Lingkaran luar dengan bayangan -->
-                <div class="absolute top-[-10px] left-[-10px] right-[-10px] bottom-[-10px] bg-slate-100 rounded-full shadow-md inset-shadow"></div>
+                <div
+                  class="absolute top-[-10px] left-[-10px] right-[-10px] bottom-[-10px] bg-slate-100 rounded-full shadow-md inset-shadow"
+                ></div>
                 <!-- Lingkaran putih untuk icon -->
-                <div class="relative z-10 flex items-center justify-center bg-white h-[150px] w-[150px] rounded-full shadow-md">
-                  <UserGroupIcon class="aspect-square w-12 h-12 text-red-400" alt="Profile icon" />
+                <div
+                  class="relative z-10 flex items-center justify-center bg-white h-[150px] w-[150px] rounded-full shadow-md"
+                >
+                  <UserGroupIcon
+                    class="aspect-square w-12 h-12 text-red-400"
+                    alt="Profile icon"
+                  />
                 </div>
               </div>
             </article>
@@ -351,7 +316,7 @@
       </section>
     </div>
     <!-- Trending Section Ends -->
-  </main>
+  </div>
 </template>
 
 <script>
@@ -588,5 +553,5 @@ export default {
   height: 100%;
 }
 
-@import '../css/pages/_button.css';
+@import "../css/pages/_button.css";
 </style>
