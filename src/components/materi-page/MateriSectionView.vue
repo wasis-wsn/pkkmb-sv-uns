69c5b6586b2286fd7a1989951a63a2a4cc9066cf
WageAriel <132081758+WageAriel@users.noreply.github.com>
2024-07-15 12:56:10 +0700

    ypdate

--- src/components/materi-page/MateriSectionView.vue
+++ src/components/materi-page/MateriSectionView.vue
@@ -27,7 +27,6 @@
         class="relative z-20 flex justify-center mt-5 md:mt-36 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
         <div class="w-full max-md:px-5 top-[50px]">
           <ul class=" text-[16px] md:text-[20px] " data-aos="fade-up">
-            <!-- <li v-for="(guideline, index) in guidelines" :key="index">{{ guideline }}</li> -->
             <li v-if="materi.length > 0">
               <h3>{{ materi[0].materi }}</h3>
               <ul class="list-disc list-inside">
@@ -41,44 +40,47 @@
         class="absolute left-0 right-0 z-10 w-full max-w-full max-h-full bottom-[70px] lg:bottom-[-80px] md:bottom-[20px]" />
     </section>
     <!-- MATERI PERTAMA -->
-    <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
-      <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
-        <h2 data-aos="fade-up"
-          class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[300px] max-md:w-full">
-          <span class="max-md:max-w-full">
-            {{ materi[1].judul }}<br />PKKMB SV UNS 2024
-          </span>
-          <div class="shrink-0 mt-2.5 h-1 bg-red-400 max-md:max-w-full"></div>
-        </h2>
-        <p data-aos="fade-up"
-          class="mt-8 z-20 text-2xl text-stone-900 max-md:max-w-full max-md:text-xl text-[18px] md:text-[24px] ">
-          Hallo Vocazens! Materi yang pertama berisi<br />Tugas Twibbon sebagai tugas dalam kegiatan<br />PKKMB SV UNS
-          2024
-        </p>
-      </div>
-    </section>
-    <!-- ISI MATERI -->
-    <section
-      class="relative flex justify-center items-center -mt-[100px] pb-32 w-full text-2xl font-bold text-black bg-white">
-      <div
-        class="relative z-20 flex justify-center mt-[150px] md:mt-44 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
-        <div class="w-full max-md:px-5 top-[50px]">
-          <ul data-aos="fade-up" class=" text-[16px] md:text-[20px]">
-            <!-- <li v-for="(guideline, index) in guidelines" :key="index">{{ guideline }}</li> -->
-            <li v-if="materi.length > 0">
-              <h3>{{ materi[1].materi }}</h3>
-              <ul class="list-disc list-inside">
-                <li v-for="(isi, index) in materi[1].isi" :key="index">{{ isi }}</li>
-              </ul>
-            </li>
-          </ul>
+    <div v-for="item in filteredMateri" :key="item.judul">
+      <section class="flex gap-5 self-center  mt-[100px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
+        <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
+          <h2 data-aos="fade-up"
+            class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[300px] max-md:w-full">
+            <span class="max-md:max-w-full">
+              {{ item.judul }}<br />PKKMB SV UNS 2024
+            </span>
+            <div class="shrink-0 mt-2.5 h-1 bg-red-400 max-md:max-w-full"></div>
+          </h2>
+          <p data-aos="fade-up"
+            class="mt-8 z-20 text-2xl text-stone-900 max-md:max-w-full max-md:text-xl text-[18px] md:text-[24px] ">
+            Hallo Vocazens! Materi yang pertama berisi<br />Tugas Twibbon sebagai tugas dalam kegiatan<br />PKKMB SV UNS
+            2024
+          </p>
         </div>
-      </div>
-      <img loading="lazy" src="@/assets/Materi/Co-Card.png" alt="Decorative gradient"
-        class="absolute left-0 right-0 z-10 w-full max-w-full max-h-full bottom-[50px] lg:bottom-[-180px] md:bottom-[20px]" />
-    </section>
+      </section>
+      <!-- ISI MATERI -->
+      <section
+        class="relative flex justify-center items-center -mt-[100px] pb-32 w-full text-2xl font-bold text-black bg-white">
+        <div
+          class="relative z-20 flex justify-center mt-[150px] md:mt-44 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
+          <div class="w-full max-md:px-5 top-[50px]">
+            <ul data-aos="fade-up" class=" text-[16px] md:text-[20px]">
+
+              <li>
+                <h3>{{ item.materi }}</h3>
+                <ul class="list-disc list-inside">
+                  <li v-for="isiItem in item.isi" :key="isiItem">{{ isiItem }}</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img loading="lazy" src="@/assets/Materi/twibbon.png" alt="Decorative gradient"
+          class="absolute left-0 right-0 z-10 w-full max-w-full max-h-full bottom-[70px] lg:bottom-[-100px] md:bottom-[20px]" />
+      </section>
+    </div>
+
     <!-- MATERI KEDUA -->
-    <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
+    <!-- <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
       <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
         <h2 data-aos="fade-up"
           class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[300px] max-md:w-full">
@@ -94,14 +96,13 @@
         </p>
       </div>
     </section>
-    <!-- ISI MATERI -->
+
     <section
       class="relative flex justify-center items-center -mt-[100px] pb-32 w-full text-2xl font-bold text-black bg-white">
       <div
         class="relative z-20 flex justify-center mt-[150px] md:mt-44 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
         <div class="w-full max-md:px-5 top-[50px]">
           <ul data-aos="fade-up" class=" text-[16px] md:text-[20px]">
-            <!-- <li v-for="(guideline, index) in guidelines" :key="index">{{ guideline }}</li> -->
             <li v-if="materi.length > 0">
               <h3>{{ materi[2].materi }}</h3>
               <ul class="list-disc list-inside">
@@ -113,9 +114,9 @@
       </div>
       <img loading="lazy" src="@/assets/Materi/banggaVokasi.png" alt="Decorative gradient"
         class="absolute left-0 right-0 z-10 w-full max-w-full max-h-full bottom-[5px] lg:bottom-[-320px] md:bottom-[20px]" />
-    </section>
+    </section> -->
     <!-- MATERI KETIGA -->
-    <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
+    <!-- <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
       <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
         <h2 data-aos="fade-up"
           class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[300px] max-md:w-full">
@@ -130,15 +131,14 @@
           kegiatan<br />PKKMB SV UNS 2024
         </p>
       </div>
-    </section>
+    </section> -->
     <!-- ISI MATERI -->
-    <section
+    <!-- <section
       class="relative flex justify-center items-center -mt-[100px] pb-32 w-full text-2xl font-bold text-black bg-white">
       <div
         class="relative z-20 flex justify-center mt-[150px] md:mt-44 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
         <div class="w-full max-md:px-5 top-[50px]">
           <ul data-aos="fade-up" class=" text-[16px] md:text-[20px]">
-            <!-- <li v-for="(guideline, index) in guidelines" :key="index">{{ guideline }}</li> -->
             <li v-if="materi.length > 0">
               <h3>{{ materi[3].materi }}</h3>
               <ul class="list-disc list-inside">
@@ -150,9 +150,9 @@
       </div>
       <img loading="lazy" src="@/assets/Materi/ringkasan.png" alt="Decorative gradient"
         class="absolute left-0 right-0 z-10 w-full max-w-full max-h-full bottom-[5px] lg:bottom-[-240px] md:bottom-[20px]" />
-    </section>
+    </section> -->
     <!-- MATERI KEEMPAT -->
-    <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
+    <!-- <section class="flex gap-5 self-center  mt-[200px] max-md:flex-wrap max-sm:flex-wrap max-md:mt-10">
       <div class="flex flex-col grow shrink-0 self-start px-5 text-center basis-0 w-fit max-md:max-w-full">
         <h2 data-aos="fade-up"
           class="flex flex-col self-center max-w-full text-3xl font-bold capitalize text-neutral-900 w-[300px] max-md:w-full">
@@ -167,15 +167,14 @@
           kegiatan<br />PKKMB SV UNS 2024
         </p>
       </div>
-    </section>
+    </section> -->
     <!-- ISI MATERI -->
-    <section
+    <!-- <section
       class="relative flex justify-center items-center -mt-[100px] pb-32 w-full text-2xl font-bold text-black bg-white">
       <div
         class="relative z-20 flex justify-center mt-[150px] md:mt-44 px-8 pt-10 md:pt-20 mb-0 w-full bg-slate-100 max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[1160px] pb-10 rounded-[60px] md:px-20 md:pb-[100px]">
         <div class="w-full max-md:px-5 top-[50px]">
           <ul data-aos="fade-up" class=" text-[16px] md:text-[20px]">
-            <!-- <li v-for="(guideline, index) in guidelines" :key="index">{{ guideline }}</li> -->
             <li v-if="materi.length > 0">
               <h3>{{ materi[4].materi }}</h3>
               <ul class="list-disc list-inside">
@@ -185,15 +184,14 @@
           </ul>
         </div>
       </div>
-      <!-- DOWNLOAD MATERI -->
-    </section>
+    </section> -->
   </main>
 </template>
 
 <script>
   import AOS from "aos";
   import "aos/dist/aos.css";
-  
+
   export default {
     name: 'MateriSectionView',
 
@@ -341,6 +339,11 @@
       }
 
     },
+    computed: {
+    filteredMateri() {
+      return this.materi.slice(1, 5);
+    }
+  },
     mounted() {
       document.title = "PKKMB SV UNS - MATERI";
       AOS.init();
