3e74003ced10a9335920070a3e44ac19742263b4
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-07-15 13:23:47 +0700

    add section compomemts

--- src/components/galeri-page/GaleriSection.vue
+++ src/components/galeri-page/GaleriSection.vue
@@ -70,3 +70,50 @@
     </div>
   </section>
 </template>
+
+<script>
+import "../../css/pages/_button.css";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default {
+  name: "GaleriView",
+  data() {
+    return {
+      galeri1: [
+        {
+          src: require("@/assets/galeri/p-1.png"),
+          class: "row-span-2 sm:row-span-2 lg:row-span-3",
+        },
+        { src: require("@/assets/galeri/p-2.png"), class: "col-span-1" },
+        { src: require("@/assets/galeri/p-3.png"), class: "col-span-1" },
+        {
+          src: require("@/assets/galeri/p-1.png"),
+          class: "row-span-2 sm:row-span-2 lg:row-span-3",
+        },
+        { src: require("@/assets/galeri/p-4.png"), class: "col-span-1" },
+        { src: require("@/assets/galeri/p-5.png"), class: "col-span-1" },
+      ],
+      galeri2: [
+        {
+          src: require("@/assets/galeri/p-1.png"),
+          class: "row-span-2 sm:row-span-2 lg:row-span-3",
+        },
+        { src: require("@/assets/galeri/p-2.png"), class: "col-span-1" },
+        {
+          src: require("@/assets/galeri/p-1.png"),
+          class: "row-span-2 sm:row-span-2 lg:row-span-3 col-end",
+        },
+        { src: require("@/assets/galeri/p-3.png"), class: "col-span-1" },
+        { src: require("@/assets/galeri/p-4.png"), class: "col-span-1" },
+        { src: require("@/assets/galeri/p-5.png"), class: "col-span-1" },
+      ],
+    };
+  },
+  mounted() {
+    document.title = "PKKMB SV UNS - GALERI";
+    AOS.init();
+  },
+};
+</script>
