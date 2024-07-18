43d67ca4986a786fbd9671232a399d8ebd553d88
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-07-18 22:27:55 +0700

    update routing

--- routes/galeriRoute.php
+++ routes/galeriRoute.php
@@ -6,8 +6,8 @@ use Inertia\Inertia;
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard/galeri', [GaleriController::class, 'index'])->name('galeri');
     Route::post('/dashboard/galeri', [GaleriController::class, 'store'])->name('galeri.store');
-    Route::put('/dashboard/galeri/{galeri}', [GaleriController::class, 'update'])->name('galeri.update');
-    Route::delete('/dashboard/galeri/{galeri}', [GaleriController::class, 'destroy'])->name('galeri.destroy');
+    Route::put('/dashboard/galeri/{id}', [GaleriController::class, 'update'])->name('galeri.update');
+    Route::delete('/dashboard/galeri/{id}', [GaleriController::class, 'destroy'])->name('galeri.destroy');
 });
 
 require __DIR__ . '/auth.php';
