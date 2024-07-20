d65357be264b5bc0e391bbd1e81e57298887b5ff
WageAriel <132081758+WageAriel@users.noreply.github.com>
2024-07-20 09:04:06 +0700

    gweh risih

--- resources/js/Components/Galeri/FormGaleri.vue
+++ resources/js/Components/Galeri/FormGaleri.vue
@@ -42,13 +42,13 @@
             <BaseButtons>
                 <BaseButton
                     type="submit"
-                    color="info"
+                    color="success"
                     label="Submit"
                     @click="submit"
                 />
                 <BaseButton
                     type="reset"
-                    color="info"
+                    color="danger"
                     @click="reset"
                     outline
                     label="Reset"
