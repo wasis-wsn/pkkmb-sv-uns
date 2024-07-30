40e0716959eee180b62cc0554ada88085409c9ca
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-07-30 23:11:42 +0700

    update , Galeri Clean , CRUD Galeri supercleann

--- resources/js/Components/User/FormUser.vue
+++ resources/js/Components/User/FormUser.vue
@@ -126,15 +126,14 @@ const reset = () => {
         </FormField>
 
         <FormField label="Mahasiswa">
-      <FormControl
-        v-model="form.nama_mahasiswa"
-        :options="mahasiswa"
-        optionLabel="nama_mahasiswa"
-        optionValue="nama_mahasiswa"
-        placeholder="Pilih Mahasiswa"
-      />
-    </FormField>
-
+            <FormControl
+                v-model="form.nama_mahasiswa"
+                :options="mahasiswa"
+                optionLabel="nama_mahasiswa"
+                optionValue="nama_mahasiswa"
+                placeholder="Pilih Mahasiswa"
+            />
+        </FormField>
         <template #footer>
             <BaseButtons>
                 <BaseButton
