01547778b551a39f6dff45cf46376eb8ffd841d7
wasiswsn <137975185+wasiswsn@users.noreply.github.com>
2024-07-31 17:32:32 +0700

    up

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "ziggy-js";
+import { InertiaProgress } from "@inertiajs/progress";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
