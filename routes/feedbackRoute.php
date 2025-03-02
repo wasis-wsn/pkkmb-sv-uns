64698b5ffafd4fefcfede01ba296863675a10409
wasiswsn <wasis1219@gmail.com>
2025-03-03 02:05:28 +0700

    a

--- routes/feedbackRoute.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\FeedbackController;
-use Illuminate\Support\Facades\Route;
-Route::middleware(['auth'])->group(function () {
-    Route::post('/api/feedback', [FeedbackController::class, 'store']);
-});
-Route::get('/api/feedback/words', [FeedbackController::class, 'getWordCloudData']);
-
-require __DIR__ . '/auth.php';
