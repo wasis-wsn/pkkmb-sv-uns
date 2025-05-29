5ecf03ddade1cb9976f0de77c2db448940b08d2f
wasiswsn <wasis1219@gmail.com>
2025-05-29 19:05:34 +0700

    Update kelompokRoute.php

--- routes/kelompokRoute.php
+++ routes/kelompokRoute.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\KelompokController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/dashboard/kelompok', [KelompokController::class, 'index'])->name('kelompok');
+    Route::get('/dashboard/kelompok', [KelompokController::class, 'index'])->name('kelompok.index');
     Route::post('/dashboard/kelompok', [KelompokController::class, 'store'])->name('kelompok.store');
     Route::put('/dashboard/kelompok/{id}', [KelompokController::class, 'update'])->name('kelompok.update');
     Route::delete('/dashboard/kelompok/{id}', [KelompokController::class, 'destroy'])->name('kelompok.destroy');
