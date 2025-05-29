9ebb8001f6f8584a7c158f1007f3f587ac8d3510
wasiswsn <wasis1219@gmail.com>
2025-05-29 19:10:06 +0700

    Update materiRoute.php

--- routes/materiRoute.php
+++ routes/materiRoute.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\MateriController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/dashboard/materi', [MateriController::class, 'index'])->name('materi');
+    Route::get('/dashboard/materi', [MateriController::class, 'index'])->name('materi.index');
     Route::post('/dashboard/materi', [MateriController::class, 'store'])->name('materi.store');
     Route::put('/dashboard/materi/{id}', [MateriController::class, 'update'])->name('materi.update');
     Route::delete('/dashboard/materi/{id}', [MateriController::class, 'destroy'])->name('materi.destroy');
