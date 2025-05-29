0071f6c8a4e7c8b3da42cdb3beac90a6d26a3bad
wasiswsn <wasis1219@gmail.com>
2025-05-29 20:04:50 +0700

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -63,7 +63,8 @@ return new class extends Migration {
             $table->string('password');
             $table->string('role')->default('user');
             $table->string('nama_mahasiswa')->nullable();
-            $table->foreign('nama_mahasiswa')->references('nama_mahasiswa')->on('mahasiswa')->onUpdate('cascade');
+            $table->unsignedBigInteger('mahasiswa_id')->nullable();
+            $table->foreign('mahasiswa_id')->references('id')->on('mahasiswa')->onUpdate('cascade')->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
